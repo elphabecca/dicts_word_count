c4577ff8cb8d00758ec6cbdee86791140d3008e7
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-03 12:51:17 -0700

    removed extra return line and included iteritems method for faster results and better memory usage.

--- wordcount.py
+++ wordcount.py
@@ -1,22 +1,20 @@
 # put your code here.
 
-def wordcount(filename):
-    """ """
+def print_wordcount(filename):
+    """Prints the word-count of each word in the given file. """
 
     dict_word_counts = {}
 
-    with open(filename) as da_file:
+    with open(filename) as text:
 
-        for line in da_file: 
+        for line in text: 
             line = line.rstrip()
             word_list = line.split()
 
             for word in word_list:
                 dict_word_counts[word] = dict_word_counts.get(word, 0) + 1
 
-    for word, count in dict_word_counts.items():
+    for word, count in dict_word_counts.iteritems():
         print "%s: %d" % (word, count)
 
-    return dict_word_counts
-
-wordcount("twain.txt")
+print_wordcount("twain.txt")
