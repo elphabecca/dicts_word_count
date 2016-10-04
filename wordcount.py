545706c8dc70f3c14ae9ce4ee97ccf4b1d5e99af
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-03 18:02:24 -0700

    Working on a function to format the words without puntuation

--- wordcount.py
+++ wordcount.py
@@ -1,5 +1,9 @@
 # put your code here.
 
+import sys
+
+filename = sys.argv[1]
+
 def print_wordcount(filename):
     """Prints the word-count of each word in the given file. """
 
@@ -8,13 +12,26 @@ def print_wordcount(filename):
     with open(filename) as text:
 
         for line in text: 
-            line = line.strip([ ,.:;?!()])
+            line = line.rstrip()
             word_list = line.split()
 
             for word in word_list:
+                # FORMAT WORD
                 dict_word_counts[word] = dict_word_counts.get(word, 0) + 1
 
     for word, count in dict_word_counts.iteritems():
         print "%s: %d" % (word, count)
 
-print_wordcount("twain.txt")
+def format_word(word):
+    
+    evil_chars = (",", "'")
+
+    for char in word:
+        print char
+        if char in evil_chars:
+            print char
+        else:
+            print "Skip"
+
+
+# print_wordcount(filename)
