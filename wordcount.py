d9cc6c4a07df85f036e24b7fa60213459ce74b60
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-05 16:59:39 -0700

    started working on making collections changes to dict.

--- wordcount.py
+++ wordcount.py
@@ -1,8 +1,35 @@
+# http://fellowship.hackbrightacademy.com/materials/f16a/exercises/dicts-word-count/further-study.html
+
 # put your code here.
 
 import sys
+import collections
 
 filename = sys.argv[1]
+
+# def print_wordcount(filename):
+#     """Prints the word-count of each word in the given file. """
+
+#     dict_word_counts = {}
+
+#     with open(filename) as text:
+
+#         for line in text: 
+#             line = line.rstrip()
+#             word_list = line.split()
+
+#             for word in word_list:
+
+#                 word = ''.join(char for char in word if char.isalnum())
+#                 word = word.lower()
+
+#                 dict_word_counts[word] = dict_word_counts.get(word, 0) + 1
+
+#     for word, count in dict_word_counts.iteritems():
+#         print "%s: %d" % (word, count)
+
+
+# print_wordcount(filename)
 
 def print_wordcount(filename):
     """Prints the word-count of each word in the given file. """
@@ -11,19 +38,16 @@ def print_wordcount(filename):
 
     with open(filename) as text:
 
-        for line in text: 
-            line = line.rstrip()
-            word_list = line.split()
+        text = text.read()
+        print text
 
-            for word in word_list:
+    #         for word in word_list:
 
-                word = ''.join(char for char in word if char.isalnum())
-                word = word.lower()
+    #             word = ''.join(char for char in word if char.isalnum())
+    #             word = word.lower()
 
-                dict_word_counts[word] = dict_word_counts.get(word, 0) + 1
+    #             full_count = collections.Counter(word)
 
-    for word, count in dict_word_counts.iteritems():
-        print "%s: %d" % (word, count)
+    # return full_count
 
-
-# print_wordcount(filename)
+print_wordcount(filename)
